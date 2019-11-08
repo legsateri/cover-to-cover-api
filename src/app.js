a5215d7788ccaa98570a4e215faf9388e64e12e8
Allegra Pusateri <allegrapusateri@gmail.com>
2019-11-08 14:02:51 -0600

    setup for deploy

--- src/app.js
+++ src/app.js
@@ -7,27 +7,25 @@ const { NODE_ENV } = require('./config')
 
 const app = express()
 
-const morganOption = (NODE_ENV === 'production')
-    ? 'tiny'
-    : 'common';
-
-app.use(morgan(morganOption))
-app.use(helmet())
-app.use(cors())
+app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common', {
+    skip: () => NODE_ENV === 'test',
+}));
+app.use(helmet());
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Hello, world!')
-})
+});
 
 app.use(function errorHandler(error, req, res, next) {
     let response
     if (NODE_ENV === 'production') {
-        response = { error: { message: 'server error' } }
+        response = { error: { message: 'server error' } };
     } else {
-        console.error(error)
+        console.log(error)
         response = { message: error.message, error }
     }
-    res.status(500).json(response)
-})
+    res.status(500).json(response);
+});
 
-module.exports = app
+module.exports = app;

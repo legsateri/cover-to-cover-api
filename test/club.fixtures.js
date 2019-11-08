bc15a0783cd12df16b6d7c6f5b4fa2cc98c309d0
Allegra Pusateri <allegrapusateri@gmail.com>
2019-11-08 15:47:19 -0600

    tests

---
+++ test/club.fixtures.js
@@ -0,0 +1,32 @@
+function makeClubsArray() {
+    return [
+        {
+            club_id: 1,
+            name: 'Text Club 1',
+            description: 'Test Club 1',
+            topic: 'Test Club 1',
+            currently_reading: '123456789012345',
+            next_meeting: '2029-01-22'
+        },
+        {
+            club_id: 2,
+            name: 'Text Club 2',
+            description: 'Test Club 2',
+            topic: 'Test Club 2',
+            currently_reading: '123456789012345',
+            next_meeting: '2029-01-22'
+        },
+        {
+            club_id: 3,
+            name: 'Text Club 3',
+            description: 'Test Club 3',
+            topic: 'Test Club 3',
+            currently_reading: '123456789012345',
+            next_meeting: '2029-01-22'
+        },
+    ]
+}
+
+module.exports = {
+    makeClubsArray
+}

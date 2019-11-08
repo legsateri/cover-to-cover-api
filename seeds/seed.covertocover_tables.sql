10c3d628f2b9d066bf7e7e86186d12ad7d4a842f
Allegra Pusateri <allegrapusateri@gmail.com>
2019-11-08 13:49:11 -0600

    finish migrations, seeds, server

---
+++ seeds/seed.covertocover_tables.sql
@@ -0,0 +1,56 @@
+BEGIN;
+
+TRUNCATE
+    book_clubs,
+    book_comments,
+    club_comments
+    RESTARTIDENTITY CASCADE;
+
+INSERT INTO book_clubs (
+    name, 
+    description, 
+    topic, 
+    currenty_reading, 
+    next_meeting
+    ) VALUES 
+    (
+        'UCWbLadies',
+        'Four gals worked at the writing center. Now they read things.',
+        'Whatevah we want',
+        '9781101980385',
+        '11-21-2019'
+    );
+
+INSERT INTO book_comments (
+    comment,
+    name,
+    book_id,
+    club_id
+    ) VALUES
+    (
+        'I would never have thought a great grandmothers experience would still cause trauma to her great granddaughter.',
+        'T-Money',
+        '9781101980385',
+        1
+    );
+
+INSERT INTO club_comments (
+    comment,
+    name,
+    club_id
+    ) VALUES
+    (
+        'Hey guys, what should we read next?',
+        'LeggyRo',
+        1
+    ),
+    (
+        'No idea, whose turn is it to pick, anyway?',
+        'T-Money',
+        1
+    ),
+    (
+        'Yours lol',
+        'Mag Millz',
+        1
+    );
---
+++ seeds/trunc.covertocover_tables.sql
@@ -0,0 +1,5 @@
+TRUNCATE
+    book_clubs,
+    club_comments,
+    book_comments
+    RESTART IDENTITY CASCADE;

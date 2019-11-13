2bc5ca96aacb65be0d0f486bcee6a301f26636c9
Allegra Pusateri <allegrapusateri@gmail.com>
2019-11-12 20:18:23 -0600

    deploy bug

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     PORT: process.env.PORT || 9000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DB_URL: process.env.DB_URL || 'postgresql://allegrapusateri@localhost/cover-to-cover',
+    DB_URL: process.env.DB_URL || 'postgresql://postgres@localhost/cover-to-cover',
     TEST_DB_URL: process.env.TEST_DB_URL || 'postgresql://dunder_mifflin@localhost/cover-to-cover-test',
     JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
 }
